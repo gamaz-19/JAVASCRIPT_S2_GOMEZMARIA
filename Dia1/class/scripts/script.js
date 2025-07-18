38632decfaf50eaec5cdf971753f4593959eb255
Maria Gomez <maga1731@gmail.com>
2025-07-18 12:23:37 -0500

    feat: :memo: End of introduction class

--- Dia1/class/scripts/script.js
+++ Dia1/class/scripts/script.js
@@ -4,7 +4,54 @@
 
 
 //!!!Primer comando!!!
+
+//#################################
+//######## Data Primitiva #########
+//#################################
+//Number
 console.log("Hola Mundo!");
+variable1= 3.14;
+console.log(variable1)
+console.log(typeof variable1)
+console.log(typeof (variable1)) //typeof es el tipo de caracter que la variable1 esta guardando (en este caso es NUMBER)
+
+//Concatenación de palabras
+palabra1="Campus"
+palabra2="Lands"
+palabra3=palabra1+palabra2;
+console.log(palabra3);
+
+//Ejemplo
+
+nombrePersona="Luis";
+plata=55000;
+console.log("la persona "+ nombrePersona +" tiene un saldo de " +plata + " pesos");
+
+//Booleanos
+
+booleanito1=true;
+booleanito2=false;
+console.log(typeof booleanito1);
+
+
+//Condicionales
+if (booleanito1 == true){
+    console.log("Es verdadero !!!");
+}else{
+    console.log("No es verdadewro :sadfeis:");
+}
+
+//Bucles 
+for(i=0;i<4;i=i+1){
+    console.log("Wenas");
+}
+
+holi=true;
+
+while(holi=true){
+    console.log("Wenass")
+    holi=false;
+}
 
 
 //Desarrollado por: Maria Alejandra Gomez Archila - cc.1005234916
