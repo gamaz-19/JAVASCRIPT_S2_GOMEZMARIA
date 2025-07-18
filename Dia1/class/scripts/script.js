45a35bf83d021eae63c95d25c9a865ea42ae7052
Maria Gomez <maga1731@gmail.com>
2025-07-18 11:26:14 -0500

    feat: :sparkles: First steps in JavaScript

---
+++ Dia1/class/scripts/script.js
@@ -0,0 +1,10 @@
+//#################################
+// ##########  Dia 1  #############
+//#################################
+
+
+//!!!Primer comando!!!
+console.log("Hola Mundo!");
+
+
+//Desarrollado por: Maria Alejandra Gomez Archila - cc.1005234916
