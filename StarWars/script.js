c9ea75d74df460a2523d2936e67fcbae0df57925
Maria Gomez <maga1731@gmail.com>
2025-07-29 23:19:24 -0500

    feat: :hammer: Working on printing caracters features

--- StarWars/script.js
+++ StarWars/script.js
@@ -13,8 +13,18 @@ function buscarResultados(){
         }else if (xhr.readyState ===4 && xhr.status === 200){
             try {
                 let datos = JSON.parse(xhr.responseText);
-                document.write (datos["name"]);
-                alert(datos["name"])
+                let name = (datos["name"]);
+                let height = (datos["height"]);
+                let mass = (datos["mass"]);
+                let hair_color = (datos["hair_color"]);
+                let eye_color = (datos["eye_color"]);
+                let birth_year = (datos["birth_year"]);
+                let gender = (datos["gender"]);
+                let homeworld = (datos["homeworld"]);
+                let films = (datos ["films"][0])
+                document.write (films)
+
+
             }
             catch (err){
                 console.log(err.message); //Si algo malo pasa simplemente se imprime el error
